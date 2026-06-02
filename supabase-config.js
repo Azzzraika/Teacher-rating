93e6bc96365801b0a1686db7253517aacc1721f4
Azzz <Azamatauelbek008@gmail.com>
2026-06-02 21:43:51 +0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -7,8 +7,7 @@ const SUPABASE_URL = 'https://kcxuxcawwnqsuhwfeivq.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjeHV4Y2F3d25xc3Vod2ZlaXZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MDgyODYsImV4cCI6MjA5NTI4NDI4Nn0.rY6xs_WexG8uPVrAPUuOwcfC9rhbmtXapBpvN1MCXbo';
 
 // Инициализация Supabase
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 // Admin emails
 const ADMIN_EMAILS = ['admin@example.com'];
 
